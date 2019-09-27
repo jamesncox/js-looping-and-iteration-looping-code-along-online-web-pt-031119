5118778e4ac35366a86e7531ff228357098ddc10
James Cox <jamesncox@yahoo.com>
2019-09-27 16:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Code your solutions in this file
-const 
+const
 
 function writeCards() {
 
-  
+
 }
 
 // const gifts = ["teddy bear", "drone", "doll"];
-//  
+//
 // function wrapGifts(gifts) {
 //   for (let i = 0; i < gifts.length; i++) {
 //     console.log(`Wrapped ${gifts[i]} and added a bow!`);
 //   }
-//  
+//
 //   return gifts;
 // }
 //  
