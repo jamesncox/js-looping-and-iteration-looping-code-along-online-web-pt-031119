28263f0413d218abf7015fff47fe413493cd5587
James Cox <jamesncox@yahoo.com>
2019-09-27 16:59:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const event = 'suprise'
 function writeCards(names, event) {
   let thankYouCards = []
   for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
 
   return names;
